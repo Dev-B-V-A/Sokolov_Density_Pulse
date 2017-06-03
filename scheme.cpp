a53218d6844ec821ca1fa576edc81e3a0e2167c7
vladislav <vladislav@localhost.localdomain>
2017-06-03 16:31:55 +0300

    NEW: write scheme base

--- scheme.cpp
+++ scheme.cpp
@@ -1,11 +1,38 @@
 #include "scheme.h"
+#include "solver.h"
 
 void scheme_Sokolov_Density_Pulse (gas_params *params)
 {
     const char *result_file = "report_scheme.tex";
 
-    int M;
+    int nodes_count = params->mx * params->my;
+    int half_nodes_count = (params->mx + 1) * (params->my + 1);
+    int time_steps = params->n;
+    int current_layer_t = 0;
+    double *density;
+    double *pulse;
+    double *old_density;
+    double *old_pulse;
 
+    density = (double *)malloc (half_nodes_count * sizeof (double));
+    old_density = (double *)malloc (half_nodes_count * sizeof (double));
+    pulse = (double *)malloc (2 * nodes_count * sizeof (double));
+    old_pulse = (double *)malloc (2 * nodes_count * sizeof (double));
+
+    init_H (density, params);
+    init_V (pulse, params);
+
+    for (current_layer_t = 0; current_layer_t < time_steps; current_layer_t++)
+    {
+        solve_density ();
+
+        solve_pulse();
+    }
 
     record_report (result_file);
+
+    free (density);
+    free (pulse);
+    free (old_density);
+    free (old_pulse);
 }
