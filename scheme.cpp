1fd092be50836abc8f6d845896619bed458c9f2c
vladislav <vladislav@localhost.localdomain>
2017-06-05 04:55:05 +0300

    FIX: print results and fix build

--- scheme.cpp
+++ scheme.cpp
@@ -4,6 +4,8 @@
 #include "stdlib.h"
 #include "report.h"
 #include "stdio.h"
+#include "norma.h"
+#include "function.h"
 
 void scheme_Sokolov_Density_Pulse (gas_params *params)
 {
@@ -17,6 +19,11 @@ void scheme_Sokolov_Density_Pulse (gas_params *params)
     double *old_density;
     double *pulse;
     double *old_pulse;
+    double mx = params->mx;
+    double hx = params->h_x;
+    double hy = params->h_y;
+    double t = 0;
+    double tau = params->tau;
 
     density = (double *)malloc (half_nodes_count * sizeof (double));
     old_density = (double *)malloc (half_nodes_count * sizeof (double));
@@ -29,10 +36,19 @@ void scheme_Sokolov_Density_Pulse (gas_params *params)
 
     for (current_layer_t = 1; current_layer_t < time_steps; current_layer_t++)
     {
+        t = current_layer_t * tau;
         printf (">      >       TimeStep = %d\n", current_layer_t);
         solve_density (density, old_density, half_nodes_count, pulse, nodes_count, params);
 
         solve_pulse (density, old_density, pulse, old_pulse, nodes_count, params, current_layer_t);
+
+        printf ("Time_LAYER:    # %d\n", current_layer_t);
+        printf ("Density          L2_norma = %.4f       C_norma = %.4f\n", l2_norma (density, density_, half_nodes_count, mx, hx, hy, t, 1),
+                                                                           c_norma (density, density_, half_nodes_count, mx, t, hx, hy, 1));
+        printf ("Pulse_1           L2_norma = %.4f       C_norma = %.4f\n", l2_norma (pulse, pulse_1, nodes_count, mx, hx, hy, t),
+                                                                            c_norma (pulse, pulse_1, nodes_count, mx, t, hx, hy));
+        printf ("Pulse_2           L2_norma = %.4f       C_norma = %.4f\n", l2_norma (pulse + nodes_count, pulse_2, nodes_count, mx, hx, hy, t),
+                                                                            c_norma (pulse + nodes_count, pulse_2, nodes_count, mx, t, hx, hy));
 
     }
 
