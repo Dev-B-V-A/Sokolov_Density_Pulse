1ed8d83a16f56775964e2912a57457059ba98224
vladislav <vladislav@localhost.localdomain>
2017-06-05 07:13:52 +0300

    FIX: report system fix

--- report.cpp
+++ report.cpp
@@ -15,16 +15,43 @@ void record_report (const char *file)
     fclose (fp);
 }
 
-void record_file(const char *file)
+void record_file (const char *file, double *H, int mh, double *V, int mv, int mx)
 {
     FILE *fp;
+    int i = 0;
+
     if (!(fp = fopen (file, "w")))
     {
-        printf ("Cannot open file for write report!\n");
+        printf ("Cannot open file for write data!\n");
         return;
     }
 
-    // data
+    fprintf (fp, "\n\n\n>>>>>> Density >>>>>>>\n Size = %d\n", mh);
+    for (i = 0; i < mh; i++)
+    {
+        fprintf (fp, " %f ", H[i]);
+        if (i % (mx - 1) == mx - 2)
+            fprintf (fp, "\n");
+    }
+    fprintf (fp, "\n\n\n<<<<<< Density <<<<<<<");
+
+    fprintf (fp, "\n\n\n>>>>>> Pulse >>>>>>>\n Size = %d\n", mv);
+    fprintf (fp, "I\n");
+    for (i = 0; i < mh; i++)
+    {
+        fprintf (fp, " %f ", V[i]);
+        if (i % mx == mx - 1)
+            fprintf (fp, "\n");
+    }
+    fprintf (fp, "II\n");
+    for (; i < mh; i++)
+    {
+        fprintf (fp, " %f ", V[i]);
+        if (i % mx == mx - 1)
+            fprintf (fp, "\n");
+    }
+
+    fprintf (fp, "\n\n\n<<<<<< Pulse <<<<<<<");
 
     fclose (fp);
 }
--- report.h
+++ report.h
@@ -2,6 +2,6 @@
 #define REPORT_H
 
 void record_report (const char *file);
-void record_file (const char *file);
+void record_file (const char *file, double *H, int mh, double *V, int mv, int mx);
 
 #endif // REPORT_H
