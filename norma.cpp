348ae784baa0e6a8a9bbca126b4ef106db915d33
vladislav <vladislav@localhost.localdomain>
2017-06-05 05:07:23 +0300

    FIX: replace printf

--- norma.cpp
+++ norma.cpp
@@ -1,5 +1,7 @@
 #include "norma.h"
 #include "math.h"
+#include "stdio.h"
+#include "function.h"
 
 double c_norma (double *u, double (*f)(double, double, double), int m, int row, double t, double hx, double hy, int offset)
 {
@@ -34,4 +36,15 @@ double l2_norma (double *u, double (*f)(double, double, double), int m, int row,
         sum += u[i] * val;
     }
     return hx * hy * (sum + psum / 2);
+}
+
+void print_norms (int current_layer_t, double *density, double *pulse, int nodes_count, int half_nodes_count, int mx, double hx, double hy, double t)
+{
+    printf ("Time_LAYER:    # %d\n", current_layer_t);
+    printf ("Density          L2_norma = %.4f       C_norma = %.4f\n", l2_norma (density, density_, half_nodes_count, mx, hx, hy, t, 1),
+                                                                       c_norma (density, density_, half_nodes_count, mx, t, hx, hy, 1));
+    printf ("Pulse_1           L2_norma = %.4f       C_norma = %.4f\n", l2_norma (pulse, pulse_1, nodes_count, mx, hx, hy, t),
+                                                                        c_norma (pulse, pulse_1, nodes_count, mx, t, hx, hy));
+    printf ("Pulse_2           L2_norma = %.4f       C_norma = %.4f\n", l2_norma (pulse + nodes_count, pulse_2, nodes_count, mx, hx, hy, t),
+                                                                        c_norma (pulse + nodes_count, pulse_2, nodes_count, mx, t, hx, hy));
 }
--- norma.h
+++ norma.h
@@ -4,4 +4,6 @@
 double l2_norma (double *u, double (*f) (double, double, double), int m, int row, double hx, double hy, double t, int offset = 0);
 double c_norma (double *u, double (*f) (double, double, double), int m, int row, double t, double hx, double hy, int offset = 0);
 
+void print_norms (int current_layer_t, double *density, double *pulse, int nodes_count, int half_nodes_count, int mx, double hx, double hy, double t);
+
 #endif  //NORMA_H
